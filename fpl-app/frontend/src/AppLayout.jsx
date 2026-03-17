c6bc2512cc90945cba84abbeef02a650a15d716b
Vdubey165 <vaibhav16305@gmail.com>
2026-03-18 00:40:47 +0530

    fixing useffect issue in applaylout

--- fpl-app/frontend/src/AppLayout.jsx
+++ fpl-app/frontend/src/AppLayout.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useGameweek } from "./hooks/useGameweek";
 import { AuthProvider, useAuth } from "./hooks/useAuth";
 import AuthModal from "./components/AuthModal";
